f52f22826a35a0834eef68d33da0af27680789ee
Kirti Pogra <88347992+Kra09-kp@users.noreply.github.com>
2024-07-09 11:55:22 +0530

    Update resumescoringsystem.py

--- resumescoringsystem.py
+++ resumescoringsystem.py
@@ -1,6 +1,5 @@
 from pypdf import PdfReader
-import openai
-from openai import OpenAI
+import google.generativeai as genai
 import sqlite3
 import io
 
@@ -9,66 +8,45 @@ def read_api_key(file_path):
     with open(file_path, 'r') as file:
         api_key = file.readline()
     return api_key
-openai.api_key = read_api_key("openai_api_key.txt")
+genai_api_key = read_api_key("genai_api_key.txt")
 
-client = OpenAI(api_key = openai.api_key)
+genai.configure(api_key=genai_api_key)
 
-system  = "You are a HR bot.You will be provided a resume and job description.You need to calculate total score based on given criteria without taking any assumptions.\
-    Remember, Stricktly follow that total score are less that or equal to 10."
+model = genai.GenerativeModel("gemini-1.5-pro")  # For the multi-model version
 
-assistant = "Hello! As the HR bot, your task is to assess a resume in comparison to a given job description. Here's what you need to do:\n\n\
-            You'll analyze the resume in relation to the provided job description: {doc}. Your evaluation will be based on the following criteria:\n\n\
+def prompt(doc,data):
+    return f"Hello! As the HR bot, your task is to assess a resume in comparison to a given job description. Here's what you need to do:\n\n\
+            You'll analyze the resume {data} on the basis of the job description: {doc}. Your evaluation will be based on the following criteria:\n\n\
                 -Assign a scores out of 5 if the candidate meets all the minimum qualifications.(For this section total score must not be greater than five.)\n\
                 -Assign a scores out of 3 if the candidate meets all the preferred qualifications.(For this section total score must not be greater than Three.)\n\
                 -Assign a scores out of 2 if the candidate's previous experience or projects align with the job responsibilities.(For this section total score must not be greater than Two.)\n\n\
             Finally, calculate the total score out of 10 and In last give the total score . Remember, the total score must not exceed 10.\n\n\
-            Additionally, you can offer suggestions to the HR team in the Note section. This could include a quick summary of the resume and any standout strengths or areas for improvement."
+            Additionally, you can offer suggestions to the HR team in the Note section. This could include a quick summary of the resume and any standout strengths or areas for improvement.\n\
+            Note section should start like this 'Note: '."
+
 
 def rate_resume(doc,text):
-    response = client.chat.completions.create(
-        model = "gpt-3.5-turbo",
-        temperature = 0, #high temperature mean more randomness, which we don't want in our result
-        top_p=0.05,
-        seed = 123,
-        messages = [
-            {
-                "role": "system",
-                "content" : system
-            },
-            {
-                "role": "assistant",
-                "content":assistant.format(doc = doc)},
-            {
-                "role":"user",
-                "content": f"Here is the provided resume: {text}. Examine this and give the total score based on the given criteria.Remember, the total score must not exceed 10."
-            }
-        ]
-    )
-    return response.choices[0].message.content
+    query = prompt(doc,text)
+    response = model.generate_content(query)
+    return response.text
 
-def find_score(text):
-    response = client.chat.completions.create(
-        model = "gpt-3.5-turbo",
-        temperature = 0, #high temperature mean more randomness, which we don't want in our result
-        top_p=1,
-        messages = [
-            {
-                "role": "assistant",
-                "content":"Extract what is the total score in the text and provide it without any explanation and other stuff. Give the total score as a number(0 to 10).\
-                    If the total score is greater than 10, then give your total score which you think it should be based on the given criteria.\n\n\
-                        Remember, the total score must not exceed 10.\n\n\
-                        Here is the given criteria:\n\n\
-                            -Assign a scores out of 5 if the candidate meets all the minimum qualifications.(For this section total score must not be greater than five.)\n\
-                            -Assign a scores out of 3 if the candidate meets all the preferred qualifications.(For this section total score must not be greater than Three.)\n\
-                            -Assign a scores out of 2 if the candidate's previous experience or projects align with the job responsibilities.(For this section total score must not be greater than Two.)\n\n\
-                        Finally, calculate the total score out of 10 and In last give the total score . Remember, the total score must not exceed 10."},
-            {
-                "role":"user",
-                "content": f"Here is given text {text}. Give what is the total score present in the Total Score section of the text. "
-            }
-        ]
-    )
-    return response.choices[0].message.content
+
+def prompt2(jd,text):
+    return f"Extract what is the total score in the {text} and provide it without any explanation and other stuff. Give the total score as a number(0 to 10).\
+If the total score is greater than 10, then give your total score which you think it should be based on the given criteria.\n\n\
+Remember, the total score must not exceed 10.\n\n\
+Here is the job description {jd}\
+Here is the given criteria:\n\n\
+    -Assign a scores out of 5 if the candidate meets all the minimum qualifications.(For this section total score must not be greater than five.)\n\
+    -Assign a scores out of 3 if the candidate meets all the preferred qualifications.(For this section total score must not be greater than Three.)\n\
+    -Assign a scores out of 2 if the candidate's previous experience or projects align with the job responsibilities.(For this section total score must not be greater than Two.)\n\n\
+Finally, calculate the total score out of 10 and In last give the total score . Remember, the total score must not exceed 10."
+            
+
+def find_score(doc,text):
+    query = prompt2(doc,text)
+    response = model.generate_content(query)
+    return response.text
 
 # Find the start of the "Note" section
 def extract_note(response_text):
